ab0c5e944e8e17d58344067ae71e0145ff621beb
siddarthsingotam <siddarthsingotam1213@gmail.com>
2025-04-25 12:19:39 +0300

    kafka dummy data update

--- backend/producer.py
+++ backend/producer.py
@@ -11,59 +11,6 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-# Example message
-
-message = {
-    "player_id": 1234,
-    "heart_rate_bpm": 110,
-    "position_x": 53.2734,
-    "position_y": -7.3222,
-}
-
-message2 = {
-    "type": "object",
-    "properties": {
-        "timestamp": {
-            "type": "string",
-            "format": "date-time"
-        },
-        "sensorId": {
-            "type": "string"
-        },
-        "type": {
-            "type": "string",
-            "enum": ["heartbeat", "oxygen_saturation", "accelerometer"]
-        },
-        "data": {
-            "type": "object",
-            "properties": {
-                "heartbeat": {
-                    "type": "number"
-                },
-                "oxygen_saturation": {
-                    "type": "number"
-                },
-                "acceleration": {
-                    "type": "object",
-                    "properties": {
-                        "x": {"type": "number"},
-                        "y": {"type": "number"},
-                        "z": {"type": "number"}
-                    }
-                },
-                "angularVelocity": {
-                    "type": "object",
-                    "properties": {
-                        "roll": {"type": "number"},
-                        "pitch": {"type": "number"}
-                    }
-                }
-            }
-        }
-    },
-    "required": ["timestamp", "sensorId", "type", "data"]
-}
-
 
 # Create heartbeat data
 def create_heartbeat_data():
@@ -72,7 +19,7 @@ def create_heartbeat_data():
         "sensorId": "sensor-12324",
         "type": 'heartbeat',
         "data": {
-            "heartbeat": random.randint(50, 200) # Simulates heartbeat in a specific range for example
+            "heartbeat": random.randint(50, 200)  # Simulates heartbeat in a specific range for example
         }
     }
 
@@ -92,6 +39,68 @@ def create_accelerometer_data():
                 "roll": random.uniform(-0.1, 0.1),
                 "pitch": random.uniform(-0.1, 0.1)
             }
+        }
+    }
+
+
+def hr_data():
+    return {
+        "HR_data": {
+            "rrData": [384],
+            "Movesense_series": "174630000192",
+            "Timestamp_UTC": 1743460235,
+            "average_bpm": 108.7813
+        }
+    }
+
+
+def ecg_data():
+    return {
+        "ECG_data": {
+            "Samples": [-62342, -51680, -43311, -35942, -29149, -23343, -18437, -14248, -10712, -7628, -4838, -2323,
+                        -90, 1953, 3926, 5939],
+            "Movesense_series": "174630000192",
+            "Timestamp_UTC": 1743460235,
+            "Timestamp_ms": 29889
+        }
+    }
+
+
+def imu9_data():
+    return {
+        "IMU9_data": {
+            "Movesense_series": "174630000192",
+            "Timestamp_UTC": 1743460235,
+            "Timestamp_ms": 30348,
+            "ArrayAcc": [
+                {"x": 8.198, "y": -5.47, "z": 1.235},
+                {"x": 8.279, "y": -5.37, "z": 1.278},
+                {"x": 8.126, "y": -5.296, "z": 1.251},
+                {"x": 7.935, "y": -5.202, "z": 1.232}
+            ],
+            "ArrayGyro": [
+                {"x": 2.8, "y": 6.3, "z": 5.6},
+                {"x": 0.56, "y": 9.52, "z": 8.82},
+                {"x": -1.75, "y": 7.0, "z": 11.2},
+                {"x": 1.68, "y": -0.63, "z": 11.06}
+            ],
+            "ArrayMagn": [
+                {"x": 42.6, "y": -28.5, "z": 3.45},
+                {"x": 44.1, "y": -27.45, "z": 5.7},
+                {"x": 43.35, "y": -28.2, "z": 4.05},
+                {"x": 43.2, "y": -25.05, "z": 3.3}
+            ]
+        }
+    }
+
+
+def gnss_data():
+    return {
+        "GNSS_data": {
+            "GNSS_sensor_ID": "self.device_id",
+            "Date": "date",
+            "Latitude": "lat",
+            "Longitude": "lon"
         }
     }
 
@@ -128,5 +137,3 @@ if __name__ == "__main__":
 #
 # # Close the producer connection
 # producer.close()
-
-
